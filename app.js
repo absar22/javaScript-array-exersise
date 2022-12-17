a749dcc58bf722a15deeae81dd3141914ba37a4e
Absar Ahmad <absarahmad137@gmail.com>
2022-12-17 11:30:46 +0530

    Solution

--- app.js
+++ app.js
@@ -10,3 +10,23 @@
 // camelize("list-style-image") == 'listStyleImage';
 // camelize("-webkit-transition") == 'WebkitTransition';
 // P.S. Hint: use split to split the string into an array, transform it and join back.
+
+
+function camelize(str){
+  let newString = str.split('-')
+  newString = newString.map(
+    function(word, index){
+      if(index === 0){
+        return word.toLowerCase()
+      }else{
+        return word[0].toUpperCase() + word.slice(1).toLowerCase()
+      }
+    }
+  )
+  return newString.join('')
+}
+
+console.log(camelize('This-Word')) 
+console.log(camelize('This-Word-letter')) 
+console.log(camelize('This-is-my-name')) 
+console.log(camelize('This-IS-my-name')) 
