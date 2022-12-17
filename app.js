27ed8acc373410bc39dec7466d95398fa9a722ba
Absar Ahmad <absarahmad137@gmail.com>
2022-12-17 12:17:41 +0530

    js array exersise question 4

--- app.js
+++ app.js
@@ -79,3 +79,13 @@ function filterRange(arr, a, b){
 
  alert( arr ); // [2, 4, 3, 1]
 
+
+ 
+
+//  Sort in decreasing order
+//  importance: 4
+//  let arr = [5, 2, 1, -10, 8];
+ 
+//  // ... your code to sort it in decreasing order
+ 
+//  alert( arr ); // 8, 5, 2, 1, -10
